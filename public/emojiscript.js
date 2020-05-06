b77048c7ae6d56a2a461e16922daaf6daa660ac0
MathiasWP <mathiaswpicker@gmail.com>
2020-05-06 11:06:56 +0200

    emojinit

---
+++ public/emojiscript.js
@@ -0,0 +1,253 @@
+(async () => {
+  /**
+   * Front-page
+   */
+  let activeEmoji;
+
+  const browseButton = document.querySelector("#let-me-browse");
+
+  browseButton.addEventListener("click", () => {
+    document.querySelector("#emoji-frontpage").classList.add("uuuuup-you-go");
+    focusEmoji(activeEmoji);
+  });
+
+  /**
+   * Fetching emojis
+   */
+
+  const response = await fetch(`${location.origin}/../emojis.json`, {
+    method: "GET",
+    mode: "cors",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    referrerPolicy: "no-referrer",
+  });
+
+  const EMOJIS = await response.json();
+
+  /**
+   * Helper function
+   */
+
+  const focusEmoji = (el) =>
+    document.getElementById(el.name.split(" ").join("_").split("’").join("--")).focus();
+
+  /**
+   * Finding emoji if in URL
+   */
+
+  const urlEmoji =
+    EMOJIS.find((emoji) => `#${emoji.name}` === decodeURI(location.hash)) ||
+    EMOJIS[Math.floor(Math.random() * EMOJIS.length)];
+
+  if (urlEmoji) {
+    if (urlEmoji.hasOwnProperty("emoji")) {
+      browseButton.textContent = `Show me my ${urlEmoji.name}-project ${urlEmoji.emoji}`;
+    }
+    browseButton.classList.remove("transparent");
+  }
+
+  activeEmoji = urlEmoji;
+
+  /**
+   * Components
+   */
+
+  const emojiShowcase = new Reef("#all-emojis", {
+    template: function (props) {
+      return `${props.emojis
+        .map(
+          (emoji) =>
+            `<span class="emoji-in-showcase" tabindex="0" id="${emoji.name
+              // All this mumbo-jumbo is to make the id names better.
+              // Change out empty spaces with underscores, and change out apostrophes with --.
+              .split(" ")
+              .join("_")
+              .split("’")
+              .join("--")}">${emoji.emoji}</span>`
+        )
+        .join("")}`;
+    },
+    data: {
+      emojis: EMOJIS,
+    },
+  });
+
+  emojiShowcase.render();
+
+  const emojiComponent = new Reef("#emoji-result", {
+    data: {
+      emoji: activeEmoji,
+    },
+    template: function (props) {
+      return `
+    <div class="emoji-result-component">
+    <h3 id="emoji-name">${props.emoji.name}</h3>
+    <h2 id="emoji">${props.emoji.emoji}</h2>
+    <section class="project-languages">
+      Languages used:
+      <span id="languages">${props.emoji.languages.join(", ")}</span>
+    </section>
+    <p id="project-desc">
+      ${props.emoji.project_description}
+    </p>
+    ${
+      props.emoji.project_tips.length > 1
+        ? `<p class="project-tips">Tips:
+          <span id="tips">${props.emoji.project_tips}</span>
+          </p>`
+        : ""
+    }
+  </div>
+    `;
+    },
+  });
+
+  emojiComponent.render();
+
+  const searchComponent = new Reef(".search-engine", {
+    template: function (props) {
+      return `
+      <input list="search-input" name="search-input" id="emoji-search" placeholder="Type in any emoji here...">
+      <datalist id="search-input">
+      ${props.emojis.map((emoji) => `<option value="${emoji.name}" tabindex="0">`).join("")}
+      </datalist>
+      
+      <button>🔍</button>`;
+    },
+    data: {
+      emojis: EMOJIS,
+    },
+  });
+
+  searchComponent.render();
+
+  /**
+   * UX: Rendering the emojis the user selects
+   */
+
+  document.querySelector("#all-emojis").addEventListener("click", (e) => {
+    if (e.target.className !== "emoji-in-showcase") return;
+
+    /**
+     * HERE I CANNOT UNDERSTAND WHAT'S WRONG
+     */
+
+    console.log(e.target.id);
+    activeEmoji = EMOJIS.find(
+      (emoji) =>
+        emoji.name.toLowerCase() === e.target.id.split("_").join(" ").split("--").join("’").toLowerCase()
+    );
+
+    console.log(activeEmoji);
+
+    Object.assign(emojiComponent.data.emoji, activeEmoji);
+
+    location.hash = activeEmoji.name;
+  });
+
+  /**
+   * Choosing a random emoji
+   */
+
+  document.querySelector("#lazy").addEventListener("click", () => {
+    activeEmoji = EMOJIS[Math.floor(Math.random() * EMOJIS.length)];
+
+    Object.assign(emojiComponent.data.emoji, activeEmoji);
+
+    location.hash = activeEmoji.name;
+
+    focusEmoji(activeEmoji);
+  });
+
+  /**
+   * Enabling scrolling through emojis with arrow-keys
+   */
+  document.querySelector("#all-emojis").addEventListener("keydown", (e) => {
+    if (e.target.className !== "emoji-in-showcase") return;
+
+    switch (e.key) {
+      case "Tab":
+        activeEmoji = EMOJIS.find(
+          (emoji) =>
+            emoji.name.toLowerCase() === e.target.id.split("_").join(" ").split("--").join("’").toLowerCase()
+        );
+        activeEmoji = e.shiftKey
+          ? EMOJIS[EMOJIS.indexOf(activeEmoji) - 1]
+          : EMOJIS[EMOJIS.indexOf(activeEmoji) + 1];
+        break;
+      case "Enter":
+        activeEmoji = EMOJIS.find(
+          (emoji) =>
+            emoji.name.toLowerCase() === e.target.id.split("_").join(" ").split("--").join("’").toLowerCase()
+        );
+        Object.assign(emojiComponent.data.emoji, activeEmoji);
+        location.hash = activeEmoji.name;
+        break;
+      case "ArrowRight":
+        const emojiToRight = EMOJIS[EMOJIS.indexOf(activeEmoji) + 1];
+        if (!emojiToRight) return;
+        Object.assign(emojiComponent.data.emoji, emojiToRight);
+        location.hash = emojiToRight.name;
+        activeEmoji = emojiToRight;
+        focusEmoji(activeEmoji);
+        break;
+      case "ArrowLeft":
+        const emojiLeft = EMOJIS[EMOJIS.indexOf(activeEmoji) - 1];
+        if (!emojiLeft) return;
+        Object.assign(emojiComponent.data.emoji, emojiLeft);
+        location.hash = emojiLeft.name;
+        activeEmoji = emojiLeft;
+        focusEmoji(activeEmoji);
+        break;
+      case "ArrowDown":
+        e.preventDefault();
+        const emojiDown = EMOJIS[EMOJIS.indexOf(activeEmoji) + 9];
+        if (!emojiDown) return;
+        Object.assign(emojiComponent.data.emoji, emojiDown);
+        location.hash = emojiDown.name;
+        activeEmoji = emojiDown;
+        focusEmoji(activeEmoji);
+        break;
+      case "ArrowUp":
+        e.preventDefault();
+        const emojiUp = EMOJIS[EMOJIS.indexOf(activeEmoji) - 9];
+        if (!emojiUp) return;
+        Object.assign(emojiComponent.data.emoji, emojiUp);
+        location.hash = emojiUp.name;
+        activeEmoji = emojiUp;
+        focusEmoji(activeEmoji);
+        break;
+    }
+  });
+
+  /**
+   * Enabling the search-form
+   */
+
+  const searchEngine = document.querySelector(".search-engine");
+  const inputForm = document.querySelector("#emoji-search");
+
+  searchEngine.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    activeEmoji = EMOJIS.find(
+      (emoji) =>
+        emoji.emoji === inputForm.value ||
+        emoji.tags.includes(inputForm.value) ||
+        emoji.aliases.includes(inputForm.value) ||
+        emoji.name.toLowerCase() === inputForm.value.toLowerCase()
+    );
+
+    if (!activeEmoji) {
+      alert("no emoji found");
+      return;
+    }
+
+    Object.assign(emojiComponent.data.emoji, activeEmoji);
+    location.hash = activeEmoji.name;
+    focusEmoji(activeEmoji);
+  });
+})();
---
+++ readme.md
@@ -0,0 +1,39 @@
+# The Emoji Project.
+
+👋 and welcome to the emoji project!
+
+The Emoji Project serves one purpose: Giving web-developers of all skill-levels inspirations for projects they create!
+
+The Emoji Project is open-source and purely created by submits from people like you 🤝❤️
+
+### To submit an emoji, do one of the two;
+
+1. Create an issue and submit your emoji there.
+
+2. Do a pull-request of the repo, insert your submission into the `emojis.json` file, and then afterwards submit your change as a pull-request.
+
+### Information needed to submit an emoji project (follow this template):
+
+```
+{
+  "emoji": "🎰",
+  "name": "slot machine",
+  "category": "Activities",
+  "aliases": ["slot_machine"],
+  "tags": ["HTML", "CSS", "JavaScript", "Game"],
+  "languages": ["HTML", "CSS", "JavaScript"],
+  "difficulty": "medium",
+  "project_description": "Create a slot-machine from scratch with HTML, CSS and Vanilla JavaScript. Bonus points if you include the usage of currency in your slot-machine.",
+  "project_tips": "Use the Math.random() function to randomly loop through the different slots."
+}
+```
+
+**NOTE:** Do NOT change the "emoji", "name", "category" or "aliases" properties. Your submission will not be accepted if you do. Also do not re-submit an already created project.
+
+### What the different properties must include:
+
+- Tags: Some tags explaining what the project is about.
+- Languages: The programming-languages used in the project.
+- Difficulty: The skill-level required for the challenge. Try to be as objective as possible.
+- Project description: A text explaining what the project should be.
+- Project tips (voluntary): In case you want to give some tips for programming the project.
